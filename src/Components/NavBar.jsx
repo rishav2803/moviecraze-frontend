b23fb19561955236ae0c9e8c48bb8100e5d6af8b
umesh2003 <umeshhiwarale3@gmail.com>
2023-09-22 18:35:08 +0530

    Changed the navbar

--- src/Components/NavBar.jsx
+++ src/Components/NavBar.jsx
@@ -1,13 +1,12 @@
-import {Box} from "@chakra-ui/react";
+import { Box } from "@chakra-ui/react";
+import { themeConfig } from "../utils/themeConfig";
 
 function NavBar() {
-  return(
-    <Box bg={"red"}>
+  return (
+    <Box bg={themeConfig.NavbgColor}>
       <h1>Hello here will come the nabar</h1>
-    </Box> 
+    </Box>
   );
 }
-
-
 
 export default NavBar;
--- src/Sections/Hero.jsx
+++ src/Sections/Hero.jsx
@@ -1,14 +1,12 @@
-import {Box} from "@chakra-ui/react";
-import NavBar from "../Componentes/NavBar";
+import { Box } from "@chakra-ui/react";
+import NavBar from "../Components/NavBar";
 
 function Hero() {
-  return(
-    <Box bg={"blue"} h={"50vh"}>
-      <NavBar/>
-    </Box> 
+  return (
+    <Box bg={"blue"} h={"100vh"}>
+      <NavBar />
+    </Box>
   );
 }
-
-
 
 export default Hero;
